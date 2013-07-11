3b5d872e97fd7c9ffd14889c22e516c4d1c5d9fc
Andrew Mleczko <andrew@mleczko.net>
2013-07-11 16:58:51 +0200

    fix

--- js/xeditable/__init__.py
+++ js/xeditable/__init__.py
@@ -8,7 +8,7 @@ bootstrap_editable = Group([
                         Resource(library, 
                               'bootstrap-editable/js/bootstrap-editable.js',
                               minified='bootstrap-editable/js/bootstrap-editable.min.js',
-                              depends=bootstrap),
+                              depends=[bootstrap]),
                         Resource(library, 
                               'bootstrap-editable/css/bootstrap-editable.css'),
                         ])
@@ -17,7 +17,7 @@ jquery_editable = Group([
                         Resource(library, 
                               'jquery-editable/js/jquery-editable-poshytip.js',
                               minified='jquery-editable/js/jquery-editable-poshytip.min.js',
-                              depends=jquery),
+                              depends=[jquery]),
                         Resource(library, 
                               'jquery-editable/css/jquery-editable.css'),
                         ])
@@ -26,7 +26,7 @@ jqueryui_editable = Group([
                         Resource(library, 
                               'jqueryui-editable/js/jqueryui-editable.js',
                               minified='jqueryui-editable/js/jqueryui-editable.min.js',
-                              depends=jquery),
+                              depends=[jquery]),
                         Resource(library, 
                               'jqueryui-editable/css/jqueryui-editable.css'),
                         ])
